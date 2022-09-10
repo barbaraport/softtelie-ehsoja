563acb1bfe44e32e092ec41eec719ffb2fbdf951
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-09-10 15:42:46 -0300

    filling and resizing the raw images

--- src/server/imageRecognition/resources/rawData/image_treatment.py
+++ src/server/imageRecognition/resources/rawData/image_treatment.py
@@ -4,39 +4,35 @@ import numpy as np
 from PIL import Image
 
 
-def fill_from_folder(folder_name):
+def fill_and_resize_from_folder(folder_name):
     images_path = os.listdir(folder_name)
 
     for image_path in images_path:
         image_to_resize = cv2.imread(folder_name + "\\" + image_path)
-        height, width = image_to_resize.shape[:2]
+        original_image_height, original_image_width = image_to_resize.shape[:2]
 
-        blank_image = np.zeros((1500, 1500, 3), np.uint8)
+        blank_image = np.zeros((1100, 1100, 3), np.uint8)
         blank_image[:, :] = (0, 0, 0)
-        bg_height, bg_width = blank_image.shape[:2]
+        blank_image_height, blank_image_width = blank_image.shape[:2]
 
-        l_img = blank_image.copy()
+        filled_image = blank_image.copy()
 
-        y_offset = round((bg_height - height) / 2)
-        x_offset = round((bg_width - width) / 2)
-        l_img[y_offset: y_offset + height, x_offset: x_offset + width] = image_to_resize.copy()
+        y_offset = round((blank_image_height - original_image_height) / 2)
+        x_offset = round((blank_image_width - original_image_width) / 2)
 
-        cv2.imwrite("../train_data_fill/images/" + image_path, l_img)
+        filled_image[y_offset: y_offset + original_image_height, x_offset: x_offset + original_image_width]\
+            = image_to_resize.copy()
 
-
-def resize_from_folder(folder_name):
-    images_paths = os.listdir(folder_name)
-
-    for imagePath in images_paths:
-        image = Image.open(folder_name + "/" + imagePath)
-
-        resized_image = image.resize((256, 768), Image.Resampling.LANCZOS)
-
-        resized_image.save("../train_data_resize/images/" + imagePath)
+        save(image_path, filled_image)
 
 
-fill_from_folder("01_SELECT21")
-fill_from_folder("01_SELECT22")
+def resize(image):
+    return image.resize((512, 512), Image.Resampling.LANCZOS)
 
-resize_from_folder("01_SELECT21")
-resize_from_folder("01_SELECT22")
+
+def save(image_name, image):
+    cv2.imwrite("../trainData/images/" + image_name, image)
+
+
+fill_and_resize_from_folder("01_SELECT21")
+fill_and_resize_from_folder("01_SELECT22")
