3185b0d258858d0ee601b046f70be7b044a355ab
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-09-10 15:19:20 -0300

    changing folder that includes the image treatment code

--- src/server/imageRecognition/resources/rawData/image_treatment.py
+++ src/server/imageRecognition/resources/rawData/image_treatment.py
@@ -1,6 +1,7 @@
+import os
 import cv2
 import numpy as np
-import os
+from PIL import Image
 
 
 def fill_from_folder(folder_name):
@@ -10,7 +11,7 @@ def fill_from_folder(folder_name):
         image_to_resize = cv2.imread(folder_name + "\\" + image_path)
         height, width = image_to_resize.shape[:2]
 
-        blank_image = np.zeros((1100, 1100, 3), np.uint8)
+        blank_image = np.zeros((1500, 1500, 3), np.uint8)
         blank_image[:, :] = (0, 0, 0)
         bg_height, bg_width = blank_image.shape[:2]
 
@@ -23,5 +24,19 @@ def fill_from_folder(folder_name):
         cv2.imwrite("../train_data_fill/images/" + image_path, l_img)
 
 
+def resize_from_folder(folder_name):
+    images_paths = os.listdir(folder_name)
+
+    for imagePath in images_paths:
+        image = Image.open(folder_name + "/" + imagePath)
+
+        resized_image = image.resize((256, 768), Image.Resampling.LANCZOS)
+
+        resized_image.save("../train_data_resize/images/" + imagePath)
+
+
 fill_from_folder("01_SELECT21")
 fill_from_folder("01_SELECT22")
+
+resize_from_folder("01_SELECT21")
+resize_from_folder("01_SELECT22")
